a85d038aeabbc552d38be2ba2eb9991e762e5875
rafael-estevam <rafael_estevan15@hotmail.com>
2022-03-14 19:55:11 -0300

    Atualização tentativas/dificuldade

--- mentalist.js
+++ mentalist.js
@@ -1,47 +1,71 @@
-function Chutar() {
+function atualizaDificuldade() {
+    let select = document.querySelector("#dificuldade");
+    let dificuldadeOptions = select.options[select.selectedIndex];
+  
+    let dificuldade = dificuldadeOptions.value;
+  
+    switch (dificuldade) {
+      case "1":
+        totalTentativas = 15;
+        break;
+      case "2":
+        totalTentativas = 10;
+        break;
+      case "3":
+        totalTentativas = 5;
+        break;
+    }
+    return dificuldade;
+  }
+  
+  function Chutar() {
     var chute = parseInt(document.getElementById("valor").value);
     var elementoResultado = document.getElementById("resultado");
     var maior = chute > numSecreto;
-
-    if (chute == numSecreto){
-        elementoResultado.innerHTML = "Parabéns, você acertou! o número secreto era " +numSecreto;
-    } else if (chute > 10 || chute < 0){
-        elementoResultado.innerHTML = "Por favor, digitar um número entre 0 e 10!"
-        tentativa--;
+  
+    if (chute == numSecreto) {
+      elementoResultado.innerHTML =
+        "Vejo que você acertou... O número secreto era " +
+        numSecreto +
+        "<br>" +
+        "Aceitaria um novo desafio???";
+      return Bloquear();
+    } else if (chute > 100 || chute < 0) {
+      elementoResultado.innerHTML = "Por favor, digitar um número entre 0 e 10!";
+      tentativa--;
     } else if (maior) {
-        elementoResultado.innerHTML = "Seu chute foi maior que o número secreto.";
+      elementoResultado.innerHTML = "Seu chute foi maior que o número secreto.";
     } else {
-        elementoResultado.innerHTML = "Seu chute foi menor que o número secreto.";
+      elementoResultado.innerHTML = "Seu chute foi menor que o número secreto.";
     }
-
+  
     return Tentativas();
-}
-
-function Tentativas(){   
+  }
+  
+  function Tentativas() {
     var elementoTentativa = document.getElementById("tentativa");
-
-    elementoTentativa.innerHTML = "Tentativa " + tentativa + " de " + totalTentativas;
+  
+    elementoTentativa.innerHTML =
+      "Tentativa " + tentativa + " de " + totalTentativas;
     tentativa++;
-
-    if (tentativa>totalTentativas){
-        elementoTentativa.innerHTML = "Total de tentativas excedido! Por favor, reinicie e tente novamente.";
-        return Bloquear();
+  
+    if (tentativa > totalTentativas) {
+      elementoTentativa.innerHTML =
+        "Você realmente não é tão inteligente quanto eu pensei que fosse. Reinicie e tente novamente.";
+      return Bloquear();
     }
-}
-
-function Bloquear(){ 
+  }
+  
+  function Bloquear() {
     document.getElementById("botaoChute").disabled = true;
-}
-
-function Reiniciar(){
-    document.location.reload();   
-}
-
-var numSecreto = Math.floor(Math.random()*101);
-var totalTentativas;
-var tentativa = 1; 
-
-var select = document.getElementById("dificuldade");
-select.addEventListener('change', function(){
-    console.log(select.value)
-})
+  }
+  
+  function Reiniciar() {
+    window.location.href = window.location.href;
+  }
+  
+  var numSecreto = Math.floor(Math.random() * 101);
+  var totalTentativas;
+  var tentativa = 1;
+  let dificuldade = atualizaDificuldade();
+  
